b5f521de0618ba5db19671a153c1579bc3e0dd75
josejayallen <josejayalle2003@gmail.com>
2024-12-02 13:35:40 +0800

    fixing database connection

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
     $servername ="localhost";
     $username="root";
-    $password="allen";
+    $password="";
     $dbname="finalproj";
 
     $conn=new mysqli($servername, $username, $password, $dbname);
